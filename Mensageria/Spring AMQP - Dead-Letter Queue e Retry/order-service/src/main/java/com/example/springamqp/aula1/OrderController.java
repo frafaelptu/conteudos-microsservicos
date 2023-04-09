ea5e911f2b035896fe4998cd3dc6083384c44fe9
francisco.gomes <francisco.gomes@via.com.br>
2023-04-09 08:39:25 -0300

    Alterado projeto para inclusão de priorização por mensagem.
    x-max-priority

--- Mensageria/Spring AMQP - Dead-Letter Queue e Retry/order-service/src/main/java/com/example/springamqp/aula1/OrderController.java
+++ Mensageria/Spring AMQP - Dead-Letter Queue e Retry/order-service/src/main/java/com/example/springamqp/aula1/OrderController.java
@@ -1,47 +1,64 @@
 package com.example.springamqp.aula1;
 
-import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 
 @RestController
 @RequestMapping(value = "/v1/orders")
 public class OrderController {
 
-	@Autowired
-	private OrderRepository orders;
+    @Autowired
+    private OrderRepository orders;
 
-	@Autowired
-	private RabbitTemplate rabbitTemplate;
-	
-	@PostMapping
-	public Order create(@RequestBody Order order) {
-		orders.save(order);
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
-		OrderCreatedEvent event = new OrderCreatedEvent(order.getId(), order.getValue());
-		rabbitTemplate.convertAndSend("orders.v1.order-created", "", event);
+    @PostMapping
+    public Order create(@RequestBody Order order) {
+        orders.save(order);
 
-		return order;
-	}
+        final int priority;
 
-	@GetMapping
-	public Collection<Order> list() {
-		return orders.findAll();
-	}
+        if (order.getValue().compareTo(new BigDecimal("5000")) >= 0) {
+            priority = 10;
+        } else {
+            priority = 0;
+        }
 
-	@GetMapping("{id}")
-	public Order findById(@PathVariable Long id) {
-		return orders.findById(id).orElseThrow();
-	}
 
-	@PutMapping("{id}/pay")
-	public Order pay(@PathVariable Long id) {
-		Order order = orders.findById(id).orElseThrow();
-		order.markAsPaid();
-		return orders.save(order);
-	}
-	
+        final MessagePostProcessor processor = message -> {
+            MessageProperties properties = message.getMessageProperties();
+            properties.setPriority(priority);
+            return message;
+        };
+
+        OrderCreatedEvent event = new OrderCreatedEvent(order.getId(), order.getValue());
+        rabbitTemplate.convertAndSend("orders.v1.order-created", "", event, processor);
+
+        return order;
+    }
+
+    @GetMapping
+    public Collection<Order> list() {
+        return orders.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Order findById(@PathVariable Long id) {
+        return orders.findById(id).orElseThrow();
+    }
+
+    @PutMapping("{id}/pay")
+    public Order pay(@PathVariable Long id) {
+        Order order = orders.findById(id).orElseThrow();
+        order.markAsPaid();
+        return orders.save(order);
+    }
+
 }
